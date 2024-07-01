4e2aaf9962611d51ce45515502091400c403c51f
micmorap <michael.morapp@gmail.com>
2024-07-01 21:10:00 +0000

    20240701 Michael Mora mlops hw06

--- 06-best-practices/batch.py
+++ 06-best-practices/batch.py
@@ -17,13 +17,58 @@ def prepare_data(df, categorical):
     return df
 
 def read_data(filename, categorical):
-    df = pd.read_parquet(filename)
     
-    return prepare_data(df, categorical)
+    S3_ENDPOINT_URL = os.getenv('S3_ENDPOINT_URL')
+
+    if S3_ENDPOINT_URL is not None:
+        options = {
+            'client_kwargs': {
+                'endpoint_url': S3_ENDPOINT_URL
+            }
+        }
+
+        df = pd.read_parquet(filename, storage_options=options)
+    else:
+        df = pd.read_parquet(filename)
+
+    return prepare_data(df, categorical)    
+    
+def save_data(filename, df):
+    S3_ENDPOINT_URL = os.getenv('S3_ENDPOINT_URL')
+
+    if S3_ENDPOINT_URL is not None:
+        options = {
+            'client_kwargs': {
+                'endpoint_url': S3_ENDPOINT_URL
+            }
+        }
+
+        df.to_parquet(filename, engine='pyarrow', index=False, storage_options=options)
+        print("Archivo parquet generado con éxito!")
+    else:
+        df.to_parquet(filename, engine='pyarrow', index=False)
+        print("Archivo parquet generado con éxito!")
+
+
+def get_input_path(year, month):
+    default_input_pattern = 'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year:04d}-{month:02d}.parquet'
+    input_pattern = os.getenv('INPUT_FILE_PATTERN', default_input_pattern)
+    return input_pattern.format(year=year, month=month)
+
+
+def get_output_path(year, month):
+    default_output_pattern = 's3://nyc-duration-mike-mora/taxi_type=fhv/year={year:04d}/month={month:02d}/predictions.parquet'
+    output_pattern = os.getenv('OUTPUT_FILE_PATTERN', default_output_pattern)
+    return output_pattern.format(year=year, month=month)
+
 
 
 def main(year:int, month:int):
-   # Load the model and vectorizer
+
+    input_file = get_input_path(year, month)
+    output_file = get_output_path(year, month)
+
+    # Load the model and vectorizer
     try:
         with open('model.bin', 'rb') as f_in:
             dv, model = pickle.load(f_in)
@@ -33,7 +78,7 @@ def main(year:int, month:int):
 
     print(f"The predictions to {year}-{month:02d} has started!")    
     
-    input_file = f"https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year}-{month:02d}.parquet"    
+    # input_file = f"https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year}-{month:02d}.parquet"    
     categorical = ['PULocationID', 'DOLocationID']
 
     df = read_data(input_file, categorical)
@@ -47,15 +92,19 @@ def main(year:int, month:int):
     df_predictions = pd.DataFrame(y_pred, columns=['predictions'])
     df_combined = pd.concat([df['ride_id'], df_predictions], axis=1)
     
-    output_file = f"output_hw06_files/hw06_predictions_{year}_{month:02d}.parquet"
+    # output_file = f"output_hw06_files/hw06_predictions_{year}_{month:02d}.parquet"
 
+    """
     df_combined.to_parquet(
         output_file,
         engine='pyarrow',
         compression=None,
         index=False
     )
-    
+    """
+
+    save_data(output_file, df_combined)
+
     mean_predictions = y_pred.mean()
 
     print(f"The predictions parquet file to {year}-{month:02d} has been created and saved into output_hw06_files folder!")
